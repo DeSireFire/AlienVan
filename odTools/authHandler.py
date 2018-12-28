7b54bf07e23b6f25523f68a6cb2b3b3beef0aeb3
DeSireFire <1025212779@qq.com>
2018-12-29 00:00:24 +0800

    code获取，注册

--- odTools/authHandler.py
+++ odTools/authHandler.py
@@ -3,6 +3,7 @@ onedrive 授权登陆处理器
 '''
 from requests_oauthlib import OAuth2Session
 import requests,json
+from alienVan.appConfig import token_url,oauthDict,authorize_url
 
 def get_sign_in_url():
     '''
@@ -14,4 +15,24 @@ def get_sign_in_url():
     redirect_uri=oauthDict['redirect'])
     sign_in_url, state = new_auth.authorization_url(authorize_url, prompt='login')
     return sign_in_url, state
+
+def get_token_from_code(code):
+    '''
+    得到返回链接：https://github.com/DeSireFire/AlienVan?code=M4d63b8cd-2951-1a96-fe98-f186a5c6e302&state=a13ypG9SFzKej0tQXvBiD1QPt46x9v
+    :param callback_url: 为示例链接里面的code
+    :return:
+    '''
+    myheader = {
+        'Content-Type':'application/x-www-form-urlencoded'
+    }
+    data = {
+        'client_id':oauthDict['app_id'],
+        'redirect_uri':oauthDict['redirect'],
+        'client_secret':oauthDict['app_secret'],
+        'code':code,
+        'grant_type':'authorization_code',
+    }
+    rep = requests.post(token_url,headers = myheader,data=data)
+    print(json.loads(rep.text))
+    return json.loads(rep.text)
 
