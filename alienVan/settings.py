bd1b68f19a26606ef6b7aa100e53375ff67a518d
DeSireFire <1025212779@qq.com>
2019-01-13 00:01:26 +0800

    删除用不上的app代码

--- alienVan/settings.py
+++ alienVan/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index',
-    'management',
     'celery',
     'django_celery_results',
+    'index',
+    'management',
+
 ]
 
 MIDDLEWARE = [
